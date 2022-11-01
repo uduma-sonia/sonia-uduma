1971dd81208167e6c6190412cc94f58919e7853c
uduma sonia <sohnyauduma@gmail.com>
2022-11-01 08:59:20 +0100

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ export default function Navbar() {
           Email
         </a>
         <a
-          href="https://drive.google.com/file/d/1pG7OoRejKSPibpTdNbxMMZ6ukSzE7_-r/view?usp=sharing"
+          href="https://drive.google.com/file/d/1b1x84rBHnFigcivC-Gj8yISGIuXDsD4H/view?usp=sharing"
           target="_blank"
           className="lg:hidden"
           rel="noopener noreferrer"
