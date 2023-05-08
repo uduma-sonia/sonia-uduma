463268380e7984a4ca1d9057ad16443ab5324692
uduma sonia <sohnyauduma@gmail.com>
2023-05-08 20:31:48 +0100

    updated projects list

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl pb-20">
+    <div className="mx-auto max-w-7xl pb-20 bg-[#13191a]">
       <Navbar />
       <Hero />
     </div>
