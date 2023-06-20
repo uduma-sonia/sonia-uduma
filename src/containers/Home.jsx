a0f9ab8e97f89fb780525f2299a3477a56717730
uduma sonia <sohnyauduma@gmail.com>
2023-06-20 10:55:04 +0100

    test

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import { useGetSubdomain } from "../components/useGetSubdomain";
 
 export default function Home() {
-  const domain = useGetSubdomain();
-
-  console.log(domain);
   return (
     <div className="mx-auto max-w-7xl pb-20 bg-[#13191a]">
       <Navbar />
